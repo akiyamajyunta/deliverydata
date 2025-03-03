45e9393676af26e8fe77924881aebfdaf0bb3e69
akiyamajyunta <phononiwasi@gmail.com>
2025-03-03 17:01:35 +0900

    new

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import maine from '../views/HomeView.vue'
+import maine from '@/scripts/compose/base1.vue'
+import Base1 from '@/scripts/compose/base1.vue';
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'maine',
+//       component: maine,
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/App.vue'),
+//     },
+//   ],
+// })
+const routes = [
+  {
+  path: '/',
+  name: 'base1',
+  component: Base1,
+  }
+]
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  routes,
+  history: createWebHistory()
+});
+
+
+
 
 export default router
---
+++ src/scripts/compose/base1.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>wryyyyyyyy</p>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
